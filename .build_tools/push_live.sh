fe6113581903b7a22e1e0704517caf854f43ca7d
ILYA Khlopotov <iilyak@apache.org>
2018-08-17 13:34:51 -0700

    ORIGIN_HEAD cannot be equal to LIVE_HEAD until we push

--- .build_tools/push_live.sh
+++ .build_tools/push_live.sh
@@ -5,4 +5,5 @@ set -e
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
     echo "==> Pushing to live repository"
     git push live HEAD:master
+    git log --graph --decorate --oneline | head
 fi
